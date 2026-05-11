b432ba0f0b6fffd025f0d28b14d3ac2f018e8a31
Chandan <chandan@192.168.1.6>
2026-05-11 20:23:37 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const supabaseUrl = 'https://kxepdnnzrinvyqaznpwu/settings/api-keys'
+const supabaseUrl = 'https://kxepdnnzrinvyqaznpwu.supabase.co'
 
 const supabaseKey = 'sb_publishable_FoyuG0mWOQuo0S_-VPcFHA_JzmIgTp7'
 
